1b878ad1bbb50e330ce658628db67372c9864ab2
vukolovanton <vukolovanton92@gmail.com>
2020-10-22 22:16:23 +0400

    Pagination for posts

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -7,8 +7,11 @@ import {
 	InputType,
 	Field,
 	Ctx,
+	UseMiddleware,
 } from "type-graphql";
-import { MyContext } from "src/types";
+import { MyContext } from "../types";
+import { isAuth } from "../utils/isAuth";
+import { getConnection } from "typeorm";
 
 @InputType()
 class PostInput {
@@ -22,8 +25,23 @@ class PostInput {
 export class PostResolver {
 	// Fetch all posts
 	@Query(() => [Post])
-	async posts(): Promise<Post[]> {
-		return Post.find();
+	async posts(
+		@Arg("limit") limit: number,
+		@Arg("cursor", () => String, { nullable: true }) cursor: string | null
+	): Promise<Post[]> {
+		const realLimit = Math.min(50, limit);
+
+		const qb = getConnection()
+			.getRepository(Post)
+			.createQueryBuilder("p")
+			.orderBy('"createdAt"', "DESC")
+			.take(realLimit);
+
+		if (cursor) {
+			qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) });
+		}
+
+		return qb.getMany();
 	}
 
 	// Fetch single post
@@ -34,14 +52,11 @@ export class PostResolver {
 
 	// Create single post
 	@Mutation(() => Post)
+	@UseMiddleware(isAuth)
 	async createPost(
 		@Arg("input") input: PostInput,
 		@Ctx() { req }: MyContext
 	): Promise<Post> {
-		if (!req.session!.userId) {
-			throw new Error("not authenticated");
-		}
-
 		return Post.create({
 			...input,
 			creatorId: req.session!.userId,
